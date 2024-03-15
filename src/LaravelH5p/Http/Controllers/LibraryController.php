28f393858434ca6b7377d4a69dd238df0499077e
Milkhan <Milkhan@users.noreply.github.com>
2024-03-15 16:01:10 +0300

    Update LibraryController.php

--- src/LaravelH5p/Http/Controllers/LibraryController.php
+++ src/LaravelH5p/Http/Controllers/LibraryController.php
@@ -3,7 +3,7 @@
 namespace Devleaptech\LaravelH5p\Http\Controllers;
 
 //use Src\Common\Infrastructure\Laravel\Controller;
-use Illuminate\Routing\Controller
+use Illuminate\Routing\Controller;
 use DB;
 use H5PCore;
 use Devleaptech\LaravelH5p\Eloquents\H5pContent;

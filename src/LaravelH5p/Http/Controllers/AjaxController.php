5adb94f83bf0f4c766cf6fcb6826c068da7574d0
Milkhan <Milkhan@users.noreply.github.com>
2024-03-15 16:01:35 +0300

    Update AjaxController.php

--- src/LaravelH5p/Http/Controllers/AjaxController.php
+++ src/LaravelH5p/Http/Controllers/AjaxController.php
@@ -3,7 +3,7 @@
 namespace Devleaptech\LaravelH5p\Http\Controllers;
 
 //use Src\Common\Infrastructure\Laravel\Controller;
-use Illuminate\Routing\Controller
+use Illuminate\Routing\Controller;
 use H5PEditorEndpoints;
 use Devleaptech\LaravelH5p\Eloquents\H5pContentsUserData;
 use Devleaptech\LaravelH5p\Eloquents\H5pLibrary;

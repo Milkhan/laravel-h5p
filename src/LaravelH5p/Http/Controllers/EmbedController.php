831cd53e1c5d835b5be4db5719f10a606586e975
Milkhan <Milkhan@users.noreply.github.com>
2024-03-15 16:01:21 +0300

    Update EmbedController.php

--- src/LaravelH5p/Http/Controllers/EmbedController.php
+++ src/LaravelH5p/Http/Controllers/EmbedController.php
@@ -3,7 +3,7 @@
 namespace Devleaptech\LaravelH5p\Http\Controllers;
 
 //use Src\Common\Infrastructure\Laravel\Controller;
-use Illuminate\Routing\Controller
+use Illuminate\Routing\Controller;
 use Devleaptech\LaravelH5p\Events\H5pEvent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
